ad20f07df3bb91c5f6540ce52fc9187a184d0215
VyacheslavKuzntesov <kuznetsov1997Vyacheslav@yandex.ru>
2021-10-13 15:02:25 +0300

    Update

--- DynamicArrays/Memory.h
+++ DynamicArrays/Memory.h
@@ -2,6 +2,8 @@
 
 #include"stdafx.h"
 
+template <typename T>T** allocate(const int rows, const int cols);
+template <typename T>void clear(T** arr, const int rows);
 template <typename T>T* push_back(T arr[], int& n);
 template <typename T>T** push_row_back(T** arr, int& rows, const int cols);
 template <typename T>void push_col_back(T** arr, const int rows, int& cols);
