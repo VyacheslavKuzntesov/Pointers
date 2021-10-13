46b769793c0aaba4e5d8a3d350f48f0a980efdea
VyacheslavKuzntesov <kuznetsov1997Vyacheslav@yandex.ru>
2021-10-13 17:41:34 +0300

    Static library check

---
+++ ArrayAndMemoryCheck/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"Array.h"
+#include"Memory.h"
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	FillRand(arr, n);
+	Print(arr, n);
+}
